a553fce05e8212735c4d35bea579a3e002494f83
Uzytkownik ZSIPONT <uzytkownik@example.com>
2024-04-14 11:22:50 +0200

    init

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo "Witaj PHP,jak się masz";
+
+$zmiennaTypuLiczbowego = "4";
+$zmiennaTypuString = "3";
+
+echo "zmiennaTypuliczbowego:".$zmiennaTypuLiczbowego;
+echo "<br>";
+echo "zmiennaTypuString".$zmiennaTypuString;
+echo "<br>";
+print "zmiennaTypuString; 
+echo  "<br>"
+
+echo "zmiennaTypuLiczbowego" + "zmiennaTypuString";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>To jest język html</h1>
+    
+</body>
+</html>
